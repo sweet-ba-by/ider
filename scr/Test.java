a0004e1ccb61bc8241ac0d891f728e42d4c65c1c
wanglijun <w1441583771@163.com>
2022-04-06 10:41:13 +0800

    提交

---
+++ scr/Test.java
@@ -0,0 +1,14 @@
+/**
+ * @program: ider
+ * @description: 测试
+ * @author: wlj
+ * @create: 2022-04-06 10:40
+ **/
+public class Test {
+    /*
+     *方法头
+     */
+    public static void main(String[] args) {
+        System.out.println("123");
+    }
+}
