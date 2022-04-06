da6bf4c3335b8a1becb9d4730ad03f24c66490b8
wanglijun <w1441583771@163.com>
2022-04-06 11:58:38 +0800

    提交

--- scr/Test.java
+++ scr/Test.java
@@ -10,5 +10,7 @@ public class Test {
      */
     public static void main(String[] args) {
         System.out.println("123");
+        System.out.println("456");
+        System.out.println("789");
     }
 }
